e2f35feca9bc9f7b65ae4300c719e61f58567f49
enrihg <99612570+enrihg@users.noreply.github.com>
2024-01-12 15:39:25 -0300

    more bug fixes

--- app.js
+++ app.js
@@ -2,58 +2,58 @@ const resultOutput = document.querySelector('.result-output');
 const buttons = document.querySelectorAll('button');
 let output = "";
 
+for (let button of buttons) {
+    button.addEventListener('click', (e) => calculate(e.target.dataset.value));
+}
+
 function calculate(value) {
     switch (value) {
         case "DEL":
             if (output === "ERROR") {
                 output = "";
-                console.log(`output is: ${output}`);
                 break;
             }
             output = resultOutput.innerText.slice(0, resultOutput.innerText.length - 1);
-            console.log(`output is: ${output}`);
             break;
 
         case "RESET":
             output = "";
-            console.log(`output is: ${output}`);
             break;
 
         case "=":
             if (output !== "") {
                 try {
+                    /*Alternativa a usar la función eval() */
                     const evaluate = function (string) {
                         return new Function('return ' + string)();
                     }
                     output = evaluate(output);
-                    console.log(`output is: ${output}`);
-
-                    /*Si divido sobre cero arrojo error */
-                    if (output == 'Infinity') { output = "ERROR" };
-
-                    /*Escapo los caracteres '//' */ 
-                    // if(output.includes('//')) {output= "ERROR"};
-                }
-                catch (e) {
+                } catch (e) {
                     output = "ERROR";
-                    console.log(`output is: ${output}`);
                 };
+
+                /*Dividir sobre cero arroja error */
+                output == 'Infinity' || output == '-Infinity' || Number.isNaN(output) ? output = "ERROR" : '';
             }
             break;
 
         /*Default servirá para las teclas numéricas y operadores aritméticos */
         default:
+
+            //Si dio error, la próxima tecla que presione resetea el valor de output
             if (output == "ERROR") { output = "" };
+
+            //Impide que se escriban dos caracteres consecutivos de '/' y '*'
+            if (value == '/' && resultOutput.innerText.endsWith('/')) { return }
+            if (value == '*' && resultOutput.innerText.endsWith('*')) { return }
+
             output += value;
-            console.log(`output is: ${output}`);
             break;
     }
 
     resultOutput.innerText = output;
 }
 
-for (let button of buttons) {
-    button.addEventListener('click', (e) => calculate(e.target.dataset.value));
-}
+
 
 
